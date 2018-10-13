9705496c89f16b6140a2694ace94aca250384d21
jorgeluisvera2004 <jorgeluisvera2004@gmail.com>
2018-10-13 17:12:46 -0500

    mi primer proyecto de NODE ANGULAR 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'peliculas';
+  public name: string;
+  public lastname: string;
+
+public lista: Array<string>
+
+constructor(){
+  this.lista=[];
+}
+  
+public mostrar() : void{
+  const item = `Nombre: ${this.name} - Apellido: ${this.lastname}`;
+    alert(item);
+    this.lista.push(item);
+}
+
 }
